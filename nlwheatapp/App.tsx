d4b02a41f94d3fc84ba47a87dfa340b47f17c2a7
juninhoww2 <alqadianoKira@gmail.com>
2021-10-25 19:08:14 -0300

    [update]

---
+++ nlwheatapp/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Home } from './src/screens/Home';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+import AppLoading from 'expo-app-loading';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading /> 
+  }
+
+  return (
+    <Home />
+  );
+}
+
--- node_heat/src/app.ts
+++ node_heat/src/app.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import express, { response, request } from "express";
+import express from "express";
 import { router } from "./routes";
 
 const app = express();
